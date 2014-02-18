b4b4fa6434c37411229ff67ab99b730de58625c4
Zack Stayman <z.stayman@gmail.com>
2014-02-18 16:48:19 -0500

    validations removed

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -1,7 +1,7 @@
 class Committee < ActiveRecord::Base
   has_many :assignments
   has_many :electeds, through: :assignments
-  validates :committee_id, presence: true
-  validates :name, uniqueness: true
-  validates :subcommittee, presence: true
+  # validates :committee_id, presence: true
+  # validates :name, uniqueness: true
+  # validates :subcommittee, presence: true
 end
