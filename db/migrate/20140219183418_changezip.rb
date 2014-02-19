7cb79e16b4cda24e23e980557bf51e0f37e84e92
Zack Stayman <z.stayman@gmail.com>
2014-02-19 13:37:42 -0500

    fix zip

---
+++ db/migrate/20140219183418_changezip.rb
@@ -0,0 +1,5 @@
+class Changezip < ActiveRecord::Migration
+  def change
+    change_column(:users, :zip, :string)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140215190834) do
+ActiveRecord::Schema.define(version: 20140219183418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,11 +53,11 @@ ActiveRecord::Schema.define(version: 20140215190834) do
   end
 
   create_table "users", force: true do |t|
-    t.string  "first_name"
-    t.string  "last_name"
-    t.integer "zip"
-    t.string  "password_digest"
-    t.string  "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "zip"
+    t.string "password_digest"
+    t.string "email"
   end
 
 end
